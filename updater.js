192485fe934cf2b76e9e9b0f863f06fa125b24f7
IntelSoftSystem <intelsoft14@gmail.com>
2019-04-28 16:19:07 +0200

    GH_TOKEM

---
+++ updater.js
@@ -0,0 +1,80 @@
+
+const {autoUpdater} = require('electron-updater');
+const { dialog } = require('electron')
+
+
+const { BrowserWindow, ipcMain} = require('electron')
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = "info";
+autoUpdater.autoDownload = false;
+
+exports.check = () => {
+    console.log('Checking for updates');
+    
+    
+    //autoUpdater.allowDowngrade = false;
+    autoUpdater.setFeedURL({
+        provider: "github",
+        owner: "IntelSoftSystem",
+        repo: "ElectronApp",
+        token: "0eb05fd9ddc136dd691ae4f38c675479d9cdd441"
+    })
+    autoUpdater.checkForUpdates();
+   autoUpdater.on('update-available', () => {
+       //prompt to ask user for updates 
+      let downloadProgress = 0;
+      const options = dialog.showMessageBox( null, {
+        type: 'info',
+        question: 'info',
+        buttons: ['Update', 'No'],
+        defaultId: 0,
+        title: 'Update Available',
+        message: 'update for a new version of YouViduMe is available ,Do you want to update now?'
+      },(buttonIndex) =>{
+          if (buttonIndex !== 0) return
+          autoUpdater.downloadUpdate();
+          let progressWin = new BrowserWindow({
+              width: 350,
+              height: 35,
+              useContentSize: true,
+              autoHideMenuBar: true,
+              maximizable: false,
+              fullscreen: false,
+              fullscreenable: false,
+              resizable: false
+          })
+          progressWin.loadURL('file://${__dirname}/prgress.html')
+          progressWin.on('closed', () => {
+              progressWin = null
+          })
+          ipcMain.on('download-progress-request', (e) => {
+              e.returnValue = downloadProgress
+          })
+          autoUpdater.on('download-progress', (d) => {
+              downloadProgress = d.percent
+              autoUpdater.logger.info(downloadProgress)
+
+          })
+          autoUpdater.on('update-downloaded', () => {
+              // if progressWin is 0 will close it 
+              if(progressWin) progressWin.close();
+              // prompt to ask user to quit app and install updates 
+              dialog.showMessageBox( null, {
+                type: 'info',
+                question: 'info',
+                buttons: ['Yes', 'Later'],
+                defaultId: 0,
+                title: 'Update Available',
+                message: 'update for a new version of YouViduMe is Ready ,Do you want to install now?'
+              } , (buttonIndex) => {
+                  if ( buttonIndex === 0 ) autoUpdater.quitAndInstall();
+              })
+
+
+          })
+
+      })
+      console.log(options)
+
+   })
+}
