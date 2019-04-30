46a49d779a48f6f59172a8e59df06e4841d90d12
IntelSoftSystem <49814044+IntelSoftSystem@users.noreply.github.com>
2019-04-29 17:16:20 -0700

    Delete index.js

--- app/features/welcome/styled/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Body } from './Body';
-export { default as Form } from './Form';
-export { default as Header } from './Header';
-export { default as Wrapper } from './Wrapper';
-export { default as Button } from './Button';
-export { default as Label } from './Label';
-export { default as H1 } from './H1';
-export { default as H2 } from './H2';
-export { default as Img } from './Img';
