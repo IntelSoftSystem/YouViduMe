db355bb756b6e47388397c0f52c4a1d2d387c03b
IntelSoftSystem <49814044+IntelSoftSystem@users.noreply.github.com>
2019-04-29 17:40:29 -0700

    Delete index.js

--- app/features/recent-list/styled/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ConferenceCard } from './ConferenceCard';
-export { default as ConferenceTitle } from './ConferenceTitle';
-export { default as RecentListContainer } from './RecentListContainer';
-export { default as TruncatedText } from './TruncatedText';
